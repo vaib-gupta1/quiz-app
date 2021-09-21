7b22fd453414c877a9bfb10b93a968b66d640ca1
mayaan <ayaantherock1@gmail.com>
2021-09-21 23:53:21 +0530

    add header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, createContext } from 'react'
 import CardList from './CardList'
 import '../css/app.css'
 import axios from 'axios'
+import Header from './Header'
+
+export const HeaderContext = createContext()
 
 function App() { 
   const [cards, setCards] = useState([])
@@ -9,27 +12,29 @@ function App() {
   const categoryEl = useRef()
   const amountEl = useRef()
 
-  useEffect(() => {
-    axios.get('https://opentdb.com/api_category.php')
-    .then( res => setCategories(res.data.trivia_categories) )
-  },[])
- 
-
-  function decodeString( str ) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
+  const headerContextValue = {
+    handleSubmit,
+    categories,
+    categoryEl,
+    amountEl
   }
 
-  function handleSubmit(e) { 
-    e.preventDefault()
+  useEffect(() => { 
+    axios.get('https://opentdb.com/api_category.php')
+    .then( res => setCategories(res.data.trivia_categories) ) 
+  },[])
+ 
+  
+  useEffect( renderQuestion,[])
+
+  function renderQuestion() {
     axios
-      .get('https://opentdb.com/api.php', {
-        params: {
-          amount: amountEl.current.value,
-          category: categoryEl.current.value
-        }
-      })
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
       .then( res => {
         setCards(res.data.results.map(((eachQuestion, index) => { 
           const option = [ ...eachQuestion.incorrect_answers.map( a => decodeString(a) ) , decodeString(eachQuestion.correct_answer)] 
@@ -43,29 +48,25 @@ function App() {
       })
   }
 
-  return (
-    <>
-      <form className="header" onSubmit={handleSubmit}> 
-        <div className="form-group">
-          <label htmlFor ="category"> Category </label>
-          <select id="category" ref={categoryEl}>
-            {categories.map(category => {
-              return <option value={category.id} key={category.id}>{category.name}</option>
-            })}
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor ="amount"> Number of Questions </label> 
-          <input type="number" id="amount" min="1" defaultValue="10" step="1" ref={amountEl}></input>
-        </div>
-        <div className="form-group">
-          <button className="btn">Generate</button>
-        </div>
-      </form>
+  function decodeString( str ) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) { 
+    e.preventDefault()
+    renderQuestion()
+  }
+
+  return ( 
+    <HeaderContext.Provider value = {headerContextValue} >
+      <Header />
       <div className="container">   
         <CardList cards={cards}/>
       </div>
-    </>
+      
+    </HeaderContext.Provider>
   );
 }
  
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react'
+import { HeaderContext } from './App'
+
+export default function Header() {
+  const {
+    handleSubmit,
+    categories,
+    categoryEl,
+    amountEl
+  } = useContext(HeaderContext)
+  return (
+    <form className="header" onSubmit={handleSubmit}> 
+      <div className="form-group">
+        <label htmlFor ="category"> Category </label>
+        <select id="category" ref={categoryEl}>
+          {categories.map(category => {
+            return <option value={category.id} key={category.id}>{category.name}</option>
+          })}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor ="amount"> Number of Questions </label> 
+        <input type="number" id="amount" min="1" defaultValue="10" step="1" ref={amountEl}></input>
+      </div>
+      <div className="form-group">
+        <button className="btn">Generate</button>
+      </div>
+    </form>
+  )
+}
